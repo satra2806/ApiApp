6d3865e0fff7e934985d24a087a946f1a618a5db
satra2806 <84612137+satra2806@users.noreply.github.com>
2021-10-22 00:15:04 +0530

    Nh

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('../users');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const router = require('../router');
+const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
